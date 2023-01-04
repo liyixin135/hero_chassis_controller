9d817a737e05c0a11f9c3bd5b41bb68c0d67dff0
liyixin135 <421159734@qq.com>
2023-01-04 16:47:58 +0800

    camke

---
+++ include/hero_chassis_controller.h
@@ -0,0 +1,16 @@
+//
+// Created by xin on 23-1-4.
+//
+
+#ifndef CATKIN_WS_HERO_CHASSIS_CONTROLLER_H
+#define CATKIN_WS_HERO_CHASSIS_CONTROLLER_H
+
+namespace hero_chassis_controller {
+
+    class hero_chassis_controller {
+
+    };
+
+} // hero_chassis_controller
+
+#endif //CATKIN_WS_HERO_CHASSIS_CONTROLLER_H
---
+++ src/hero_chassis_controller.cpp
@@ -0,0 +1,8 @@
+//
+// Created by xin on 23-1-4.
+//
+
+#include "hero_chassis_controller.h"
+
+namespace hero_chassis_controller {
+} // hero_chassis_controller
